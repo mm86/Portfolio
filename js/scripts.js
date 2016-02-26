54f7e32d3526792a9e3a4a10d850fa80a672283b
Madhu <madhuvanmahesh@gmail.com>
2016-02-25 17:07:57 -0800

    Indent the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
+jQuery(document).ready(function() {
+  jQuery('#about').addClass("hideme").viewportChecker({
+    classToAdd: 'visible animated fadeIn',
+    offset: 100
+  });
+});
 
-         jQuery(document).ready(function() {
-         jQuery('#about').addClass("hideme").viewportChecker({
-          classToAdd: 'visible animated fadeIn',
-          offset: 100
-         });
-         });
 
-
-         var offset = 500;
-         jQuery(window).scroll(function() {
-          if(jQuery(window).scrollTop() < offset) {
-            jQuery('#hidden-bar').fadeOut(200);
-          } else if(jQuery(window).scrollTop() > offset) {
-            jQuery('#hidden-bar').fadeIn(400);
-          }
-         });
+var offset = 500;
+jQuery(window).scroll(function() {
+  if (jQuery(window).scrollTop() < offset) {
+    jQuery('#hidden-bar').fadeOut(200);
+  } else if (jQuery(window).scrollTop() > offset) {
+    jQuery('#hidden-bar').fadeIn(400);
+  }
+});
